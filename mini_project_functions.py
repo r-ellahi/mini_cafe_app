49dd8ac07575395b1aa6fc07dde80cff1d56ad50
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-07-27 23:39:43 +0100

    update

--- mini_project_functions.py
+++ mini_project_functions.py
@@ -1,9 +1,8 @@
 import csv
-
+from pprint import pprint
 
 def whitespace():
     print('\n')
-    
 
 
 #READ FILES 
@@ -16,40 +15,36 @@ def read_csv_file (file_name, csv_to_read):
         return csv_list
 
 
-#Print files
-with open('Product_list.csv', 'r') as file:
-    csv_file = csv.DictReader(file)
-    for row in csv_file:
-        print(row)
+#PRINT FILES 
+def print_csv_file (file_name, *csv_file):
+    with open(file_name, 'r') as csv_print:
+        csv_file = csv.DictReader(csv_print)
+        for row in csv_file:
+            pprint(dict(row))
+    
 
 
 
 #Dictionary Append
-def dict_append(file_name, dict_elements, field_names):
-    with open (file_name, 'a+', newline= '') as dict_append_file:
-        dict_writer = DictWriter(dict_append_file, fieldnames = field_names)
-        dict_writer.writerow(dict_elements)
+def append_dict(file_name, dict_of_elem, field_names):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        dict_writer = DictWriter(write_obj, fieldnames=field_names)
+        # Add dictionary as wor in the csv
+        dict_writer.writerow(dict_of_elem)
+
+
+#Write in python file
+field_names = ['Id','Name','Course','City','Session']
+row_dict = {'Id': 81,'Name': 'Sachin','Course':'Maths','City':'Mumbai','Session':'Evening'}
+# Append a dict as a row in csv file
+append_dict_as_row('students.csv', row_dict, field_names)
 
 
 
-
-
-# def print_product():
-#     with open('product_list.csv','r+') as f:
-# 	    lines=f.read().splitlines()
-# 	    print(lines)
-
-
-
-# def print_csv_file(csv_file):
-#     for key, value in enumerate(csv_file):
-#         print(key, value)
-#     return key, value
-
-# def print_csv_file (file_name, csv_to_read):
-#     with open (file_name, 'r') as csv_file:
-#         csv_to_read = csv.DictReader(csv_file)
-#         csv_list = []
-#         for row in csv_to_read:
-#             csv_list.append(row) 
-#         print(csv_list)
+#Write in python file
+field_names = ['Id','Name','Course','City','Session']
+row_dict = {'Id': 81,'Name': 'Sachin','Course':'Maths','City':'Mumbai','Session':'Evening'}
+# Append a dict as a row in csv file
+append_dict_as_row('students.csv', row_dict, field_names)
