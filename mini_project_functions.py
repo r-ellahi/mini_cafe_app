bfb819cb357dc220ced54faa5ba5b4bbca9ad3d8
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-07-28 20:39:23 +0100

    funtions update

--- mini_project_functions.py
+++ mini_project_functions.py
@@ -1,11 +1,12 @@
 import csv
 from pprint import pprint
+from csv import DictWriter, DictReader
 
 def whitespace():
     print('\n')
 
 
-#READ FILES 
+# READ FILES 
 def read_csv_file (file_name, csv_to_read):
     with open (file_name, 'r') as csv_file:
         csv_to_read = csv.DictReader(csv_file)
@@ -14,18 +15,25 @@ def read_csv_file (file_name, csv_to_read):
             csv_list.append(row) 
         return csv_list
 
+# SAVE FILES 
+def save_csv_file(file_name, list_name):
+    with open(file_name, "w", newline='') as updated:
+        if list_name:
+            writer = csv.DictWriter(updated, fieldnames=list_name[0].keys())
+            writer.writeheader()
+            writer.writerows(list_name)
 
-#PRINT FILES 
+
+# PRINT FILES 
 def print_csv_file (file_name, *csv_file):
     with open(file_name, 'r') as csv_print:
         csv_file = csv.DictReader(csv_print)
         for row in csv_file:
-            pprint(dict(row))
+            print(dict(row))
     
 
 
-
-#Dictionary Append
+# Dictionary Append
 def append_dict(file_name, dict_of_elem, field_names):
     # Open file in append mode
     with open(file_name, 'a+', newline='') as write_obj:
@@ -33,18 +41,3 @@ def append_dict(file_name, dict_of_elem, field_names):
         dict_writer = DictWriter(write_obj, fieldnames=field_names)
         # Add dictionary as wor in the csv
         dict_writer.writerow(dict_of_elem)
-
-
-#Write in python file
-field_names = ['Id','Name','Course','City','Session']
-row_dict = {'Id': 81,'Name': 'Sachin','Course':'Maths','City':'Mumbai','Session':'Evening'}
-# Append a dict as a row in csv file
-append_dict_as_row('students.csv', row_dict, field_names)
-
-
-
-#Write in python file
-field_names = ['Id','Name','Course','City','Session']
-row_dict = {'Id': 81,'Name': 'Sachin','Course':'Maths','City':'Mumbai','Session':'Evening'}
-# Append a dict as a row in csv file
-append_dict_as_row('students.csv', row_dict, field_names)
