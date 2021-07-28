bfbc48ddac5779a0ce749d90b782ad986073615b
RUMAANAH_ELLAHI <r.ellahi@hotmail.com>
2021-07-28 20:41:18 +0100

    new file

--- mini_project_task4.py
+++ mini_project_task4.py
@@ -1,17 +1,15 @@
-from pprint import pprint
-from mini_project_functions import whitespace, read_csv_file, print_csv_file, append_dict
+from mini_project_functions import whitespace, read_csv_file, print_csv_file, save_csv_file, append_dict
 from csv import DictWriter, DictReader
 
-product = {}
-# courier = []
-# orders = []
+product = []
+courier = []
+orders = []
 
 product = read_csv_file('product_list.csv', product)
-# courier = read_csv_file('courier_list.csv', courier)
-# orders = read_csv_file('order.csv', orders)
+courier = read_csv_file('courier_list.csv', courier)
+orders = read_csv_file('orders.csv', orders)
 
 order_status = ['Order Confirmed', 'Preparing', 'Quality Check', 'On Route', 'Delivered', 'Unable to Deliver']
-
 
 def main_menu():
     print("""\033[33m\n\tMain Menu:\033[0m""")
@@ -23,28 +21,23 @@ def main_menu():
     """)
     
     option = int(input("""\033[33m\n\tEnter your choice here: \033[0m"""))
-
+    
     if option == 0:
-        with open('Product_list.txt', 'w') as save_p_file:
-            for save_i in product_list:
-                save_p_file.write(save_i + '\n')
-        with open('Courier_list.txt', 'w') as save_c_file:
-            for save_w in courier_list:
-                save_c_file.write(save_w + '\n')
+        save_csv_file('product_list.csv', product)
         print('Thanks for visiting!')
-        exit
+        exit()
         
     elif option == 1:
-        product()   
+        product_menu()   
     
     elif option == 2:
         courier()
     
     elif option == 3:
         orders_details()
-
-
-def product():
+        
+        
+def product_menu():
     print("""\033[33m\n\tProduct Options:\033[0m""")
     print('''
         [0] - Return to Main Menu
@@ -53,10 +46,11 @@ def product():
         [3] - Update Existing Product    
         [4] - Delete a Product''')
     user_input = int(input("""\033[33m\n\tEnter your choice here: \033[0m"""))
-
-    
+        
+        
     if user_input == 0:
         whitespace()
+        save_csv_file('product_list.csv', product)
         print('Thanks for visiting!')
         whitespace()
         exit
@@ -66,8 +60,7 @@ def product():
         whitespace()
         print_csv_file('product_list.csv', product)
         main_menu()
-        
-        
+    
     elif user_input == 2:
         whitespace()
         print("The current menu is: ")
@@ -75,17 +68,13 @@ def product():
         whitespace()
         new_product = input("What would you like to create?  ")
         product_price = float(input("Enter a price:   "))
-        
-        
         new_product_dict = {}
         new_product_dict['Product'] = new_product
         new_product_dict['Price'] = product_price
         product.append(new_product_dict)
         field_names = ['Product', 'Price']
-        dict_append('product_list.csv', new_product_dict, field_names)
+        append_dict('product_list.csv', new_product_dict, field_names)
         print('Your new product has been created!')
-        print_product()
-    
-    
-    
+        print(new_product_dict)
+        
 main_menu()
